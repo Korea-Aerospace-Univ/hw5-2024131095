45bbb356e28c71c15976166a3800fa2101cdac57
2024131095 <yeheepark@kau.kr>
2026-05-04 15:31:53 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -10,7 +10,7 @@ int main()
    for (int a = 1; 900*a<=N; a++){
        for (int b = 1; 750*b<=N; b++){
            for (int c = 1; 200*c<=N; c++){
-               if ((900*a+750*b+200*c==N) && (b%2==0) && (c<=a||c<b)){
+               if ((900*a+750*b+200*c==N) && (b%2==0) && (c<a||c<b)){
                    printf("%d %d %d \n",a,b,c);
                    found = 1;
                }
