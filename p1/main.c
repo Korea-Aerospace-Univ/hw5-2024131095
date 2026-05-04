f9de1df4b2f8262a908116049633d1637d14fca3
2024131095 <yeheepark@kau.kr>
2026-05-04 15:25:10 +0900

    Add main.c to find valid combinations of a, b, c
    Implement a program to find combinations of a, b, and c such that 900*a + 750*b + 200*c equals N, with specific conditions for b and c.

---
+++ p1/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+   int N;
+   int found = 0;
+   
+   scanf ("%d", &N);
+   
+   for (int a = 1; 900*a<=N; a++){
+       for (int b = 1; 750*b<=N; b++){
+           for (int c = 1; 200*c<=N; c++){
+               if ((900*a+750*b+200*c==N) && (b%2==0) && (c<=a||c<b)){
+                   printf("%d %d %d \n",a,b,c);
+                   found = 1;
+               }
+           }
+       } 
+   }
+    if (found == 0) {
+        printf("none\n");
+    }
+    return 0;
+}
