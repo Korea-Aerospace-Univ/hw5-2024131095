f045eb06203b5b2c5c0f2e3a423ff1da72a3651b
2024131095 <yeheepark@kau.kr>
2026-05-04 15:40:01 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
    int N;
-   int found = 0;
+   bool found = false;
    
    scanf ("%d", &N);
    
    for (int a = 1; 900*a<=N; a++){
-       for (int b = 1; 750*b<=N; b++){
+       for (int b = 2; 750*b<=N; b+=2){
            for (int c = 1; 200*c<=N; c++){
-               if ((900*a+750*b+200*c==N) && (b%2==0) && (c<a||c<b)){
+               if ((900*a+750*b+200*c==N) && (c<a||c<b)){
                    printf("%d %d %d \n",a,b,c);
-                   found = 1;
+                   found = true;
                }
            }
        } 
    }
-    if (found == 0) {
+    if (found == false) {
         printf("none\n");
     }
     return 0;
